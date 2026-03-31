7abb2c5b7a8afe729bf2188a2f1acc81a0102eca
Mohamed Hasan <Modernkille3grow@gmail.com>
2026-03-31 15:37:34 +0300

    fully made authrouter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,5 +4,8 @@ const router = express.Router()
 const authController = require("../controllers/authController.js")
 
 router.post("/sign-up", authController.registerUser)
+router.post("/sign-in", authController.signInUser)
+router.post("/sign-out", authController.signOutUser)
+router.get("/current-user", authController.getCurrentUser)
 
 module.exports = router
