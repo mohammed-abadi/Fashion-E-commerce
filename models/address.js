cc02b62a6349e850dd712d649a7556e916ea448d
Aboood105 <orcs882@gmail.com>
2026-04-01 13:16:31 +0300

    create models/address.js and finish it

---
+++ models/address.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const addressSchema = new mongoose.Schema(
+  {
+    address: {
+      type: String,
+      required: true,
+    },
+    street: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: false,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model("Address", addressSchema)
