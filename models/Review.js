c0730b40eefb4e48892d34a9080e944238d13c85
Aboood105 <orcs882@gmail.com>
2026-04-01 14:07:53 +0300

    create models/review.js and finish it

---
+++ models/Review.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+// maybe i forgot something like product
+const reviewSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model("Review", reviewSchema)
